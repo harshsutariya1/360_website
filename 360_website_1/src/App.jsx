bd2e37fa474aa42518f5b56968dde4cacef3faa2
Harsh Sutariya <hr.sutariya1@gmail.com>
2025-12-11 04:31:57 +0530

    refactor: Update import statement in App component and enhance Footer with logo

--- 360_website_1/src/App.jsx
+++ 360_website_1/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
