2f608ef48c036d353569d8e87c54559e17fc6472
Harsh Sutariya <hr.sutariya1@gmail.com>
2025-12-10 20:39:34 +0530

    css update

--- 360_website_1/tailwind.config.js
+++ 360_website_1/tailwind.config.js
@@ -5,8 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: "blob 7s infinite",
+        'fade-in-up': 'fadeInUp 0.8s ease-out forwards',
+      },
+      keyframes: {
+        blob: {
+          "0%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
+          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+          "100%": { transform: "translate(0px, 0px) scale(1)" },
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translate3d(0, 40px, 0)' },
+          '100%': { opacity: '1', transform: 'translate3d(0, 0, 0)' },
+        }
+      },
+    },
   },
   plugins: [],
 }
-
